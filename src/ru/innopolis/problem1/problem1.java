53f3a377279485e32c9ca32ab76306e5417cce7b
M4mou <54815296+M4mou@users.noreply.github.com>
2022-04-08 15:09:27 +0300

    Update problem1.java

--- src/ru/innopolis/problem1/problem1.java
+++ src/ru/innopolis/problem1/problem1.java
@@ -1,2 +1,58 @@
-// your solution here
-//fix all the errors including package errors
+//package ru.nguira;
+
+interface Handler{
+    void setNext(Handler h);
+    void handle();
+}
+class BaseHandler implements Handler{
+    Handler next = null;
+    @Override
+    public void setNext(Handler h) {
+        this.next = h;
+    }
+
+    @Override
+    public void handle() {
+        if(next != null)next.handle();
+    }
+}
+class FileHandler extends BaseHandler{
+    @Override
+    public void handle() {
+        System.out.println("Write to File");
+        super.handle();
+    }
+}
+class ConsoleHandler extends BaseHandler {
+    @Override
+    public void handle() {
+        System.out.println("Write to Console");
+        super.handle();
+    }
+}
+class ServerHandler extends BaseHandler{
+    @Override
+    public void handle() {
+        System.out.println("Send to Server");
+        super.handle();
+    }
+}
+class LoggerFramework{
+    Handler fileHandler = new FileHandler(), consoleHandler = new ConsoleHandler(), serverHandler = new ServerHandler();
+    public void setWriteToOnlyFiles(boolean b){
+        if(b)fileHandler.setNext(null);
+        else fileHandler.setNext(consoleHandler);
+    }
+    public void writeLogs(){
+        fileHandler.setNext(consoleHandler);
+        consoleHandler.setNext(serverHandler);
+        fileHandler.handle();
+    }
+}
+public class Main {
+
+    public static void main(String[] args) {
+	    LoggerFramework loggerFramework = new LoggerFramework();
+	    loggerFramework.writeLogs();
+    }
+}
